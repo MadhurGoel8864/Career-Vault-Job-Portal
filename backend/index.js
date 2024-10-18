5dfc0a3e7dad6bdfbc78702cc9f4302e084a9ad6
MadhurGoel8864 <madhur12a16072@gmail.com>
2024-10-18 14:06:32 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,14 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-// import path from "path"
+import path from "path"
 
 dotenv.config({});
 
 const app = express();
+
+const _dirname = path.resolve();
+
 
 // middleware
 app.use(express.json());
@@ -21,6 +24,7 @@ const corsOptions = {
     origin:'http://localhost:5173',
     credentials:true
 }
+
 
 app.use(cors(corsOptions));
 
@@ -33,6 +37,11 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
 
 
 app.listen(PORT,()=>{
